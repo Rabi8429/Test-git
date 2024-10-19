5584b60aed0115f854298779823c7fea10458241
Rabi <rabi.gayen@gmail.com>
2024-10-19 06:45:06 +0530

    New codes added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup (
+    name='python-web-demo-flask',
+    packages=[' Hello World'],
+    package_data= [help]
+)
+
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
